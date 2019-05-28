7ec71be1b229b129fde438ba3f2d46a5d66641d2
Holyflare <coldwar123@gmail.com>
2019-05-28 19:32:08 +0100

    new version

--- webscraper.py
+++ webscraper.py
@@ -4,14 +4,31 @@ import requests
 url = 'https://tl.net/forum/mafia/547420-72-24-midnight-sun-mafia?page=259'
 page = requests.get(url, timeout=5)
 soup = BeautifulSoup(page.content, "html.parser")
+block = soup.find_all('div', {'class': 'solid'})
+poster = soup.find_all('div', {'class': 'fpost-username'})
+posted = soup.find_all('article', {'class': ['section','forumPost']})
+
 
 
 for post in soup.findAll('article', {'class': ['section','forumPost']}):
 	for div in soup.find_all("div", {'class':'quote'}):
 		div.decompose()
-	print post.text.encode("utf-8")
+		print soup
+	# print post.text.encode("utf-8")
 
 
+
+
+
+
+
+# for post in soup.findAll('article', {'class': ['section','forumPost']}):
+# 	for div in soup.find_all("div", {'class':'quote'}):
+# 		div.decompose()
+# 	print post.text.encode("utf-8")
+
+# for poster in soup.find('div', {'class': 'fpost-username'}).find('span', recursive=False):
+# 	print poster.encode("utf-8")
 
 
 
@@ -30,4 +47,4 @@ for post in soup.findAll('article', {'class': ['section','forumPost']}):
 
 
 # for hi in content.find('div', {'class': 'fpost-username'}).find('span', recursive=False):
-	# print hi.encode("utf-8")
+# 	print hi.encode("utf-8")
