daf11bae5bd0f95ddeb0d10d471d003a3dca4b00
Holyflare <coldwar123@gmail.com>
2019-05-28 19:18:24 +0100

    got all posts

--- webscraper.py
+++ webscraper.py
@@ -4,11 +4,16 @@ import requests
 url = 'https://tl.net/forum/mafia/547420-72-24-midnight-sun-mafia?page=259'
 page = requests.get(url, timeout=5)
 soup = BeautifulSoup(page.content, "html.parser")
-block = soup.find_all('div', {'class': 'solid'})
-poster = block.find_all('div', {'class': 'fpost-username'})
-posted = block.find_all('article', {'class': ['section','forumPost']})
 
-print(poster.prettify())
+
+for post in soup.findAll('article', {'class': ['section','forumPost']}):
+	for div in soup.find_all("div", {'class':'quote'}):
+		div.decompose()
+	print post.text.encode("utf-8")
+
+
+
+
 
 
 
@@ -25,4 +30,4 @@ print(poster.prettify())
 
 
 # for hi in content.find('div', {'class': 'fpost-username'}).find('span', recursive=False):
-# 	print hi.encode("utf-8")
+	# print hi.encode("utf-8")
