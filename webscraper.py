01235ca4728f036082bfe1dd712c0b45a296258a
Holyflare <coldwar123@gmail.com>
2019-05-28 22:26:44 +0100

    look mum no hands pt2

--- webscraper.py
+++ webscraper.py
@@ -4,30 +4,36 @@ import requests
 import itertools
 import sys
 from collections import Counter
+from multiprocessing  import Pool
 
-url = 'https://tl.net/forum/mafia/547420-72-24-midnight-sun-mafia?page=259'
-page = requests.get(url, timeout=5)
-soup = BeautifulSoup(page.content, "html.parser")
-block = soup.find_all('div', {'class': 'solid'})
-poster = soup.find_all('div', {'class': 'fpost-username'})
-posted = soup.find_all('article', {'class': ['section','forumPost']})
+
 filtered = []
 usernames = []
 
+for i in range(262):
+	url = 'https://tl.net/forum/mafia/547420-72-24-midnight-sun-mafia?page=' + str(i)
+	page = requests.get(url, timeout=5)
+	soup = BeautifulSoup(page.content, "html.parser")
+	block = soup.find_all('div', {'class': 'solid'})
+	poster = soup.find_all('div', {'class': 'fpost-username'})
+	posted = soup.find_all('article', {'class': ['section','forumPost']})
 
-for post in soup.findAll('article', {'class': ['section','forumPost']}):
-	for div in soup.find_all("div", {'class':'quote'}):
-		div.decompose()
-	filtered.append(post.text.encode("utf-8").strip())
+
+
+	for post in soup.findAll('article', {'class': ['section','forumPost']}):
+		for div in soup.find_all("div", {'class':'quote'}):
+			div.decompose()
+		filtered.append(post.text.encode("utf-8").strip())
 
 
 
-for i in poster:
-	usernames.append(i.findAll('span')[0].text)
+	for i in poster:
+		usernames.append(i.findAll('span')[0].text)
 
 
 zipper = zip(usernames, filtered)
 splitList = []
+
 
 
 
@@ -40,6 +46,7 @@ def WordCount(username):
 			splitList.append(str(zipper[a][1]).split())
 
 
+
 print("please type username")
 userinput = raw_input()
 WordCount(userinput)
@@ -48,8 +55,6 @@ for sublist in splitList:
 	for item in sublist:
 		flat_list.append(item)
 print len(flat_list)
-
-
 
 
 
