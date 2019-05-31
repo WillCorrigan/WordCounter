4d55fd652f6670b5683ae07bb7cb71e36ea82ce4
Holyflare <coldwar123@gmail.com>
2019-05-31 17:40:23 +0100

    help threading

--- threadingtest.py
+++ threadingtest.py
@@ -4,8 +4,7 @@ import requests
 import itertools
 import sys
 from time import sleep
-from collections import Counter
-# from multiprocessing import Pool
+from collections import defaultdict
 import concurrent.futures
 
 ############################## Cluster Fuck Begins Here ###################################
@@ -20,26 +19,36 @@ splitList = []
 ############### Functions  ##############
 
 def generate_urls():
-    for i in range(1,11):
+    for i in range(6,7):
         all_urls.append(base_url + str(i))
     
 def scrape(url):
     res = requests.get(url)
     soup = BeautifulSoup(res.content, "html.parser")
     print(res.status_code, res.url)
+    tempusernames = []
+    tempposts = []
+    testzip = []
+
     for post in soup.findAll('article', {'class': ['section','forumPost']}):
     	for div in soup.find_all("div", {'class':'quote'}):
     		div.decompose()
-    		filtered.append(post.text.encode("utf-8").strip())
+    	tempposts.append(post.text.encode("utf-8").strip())
+    		
     for i in soup.find_all('div', {'class': 'fpost-username'}):
-    	usernames.append(i.findAll('span')[0].text)
-    			
+    	tempusernames.append(i.findAll('span')[0].text)
+    
+    zipper.append(list(zip(tempusernames, tempposts)))
+    
+  
+    		
 
 
 def WordCount(username):
-	for a in range(len(zipper)):
-		if zipper[a][0] == username:
-			splitList.append(str(zipper[a][1]).split())
+	for a in range(0, len(zipper)):
+		if zipper[0][a][0] == username:
+			splitList.append(str(zipper[0][a][1]).split())
+			print(splitList)
 
 
 
@@ -54,15 +63,14 @@ generate_urls()
 with concurrent.futures.ThreadPoolExecutor(max_workers=None) as executor: 
 	executor.map(scrape, all_urls)
 
-
-zipper = list(zip(usernames, filtered))
-
+print(zipper[0][1][1])
 playerList = ["BloodyC0bbler", "wherebugsgo", "Koshi", "Holyflare", "VisceraEyes", "Conversion", "Jockmcplop", "iGrok", "raynpelikoneet", "ruXxar", "disformation", "Artanis[Xp]", "Calix"]
 
 for playerName in range(len(playerList)):
 	# print("please type username")
 	userinput = playerList[playerName]
 	WordCount(userinput)
+	print(splitList)
 	flat_list = []
 	for sublist in splitList:
 		for item in sublist:
@@ -71,66 +79,3 @@ for playerName in range(len(playerList)):
 	print(len(flat_list))
 	splitList = []
 	flat_list = []
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# ############################## Iterates Through Every TL Mafia Page - Adds to Lists ###########################
-
-# for i in range(10):
-# 	url = 'https://tl.net/forum/mafia/547420-72-24-midnight-sun-mafia?page=' + str(i)
-# 	page = requests.get(url, timeout=5)
-# 	soup = BeautifulSoup(page.content, "html.parser")
-# 	block = soup.find_all('div', {'class': 'solid'})
-# 	poster = soup.find_all('div', {'class': 'fpost-username'})
-# 	posted = soup.find_all('article', {'class': ['section','forumPost']})
-
-
-
-# 	for post in soup.findAll('article', {'class': ['section','forumPost']}):
-# 		for div in soup.find_all("div", {'class':'quote'}):
-# 			div.decompose()
-# 		filtered.append(post.text.encode("utf-8").strip())
-
-
-# 	for i in soup.find_all('div', {'class': 'fpost-username'}):
-# 		usernames.append(i.findAll('span')[0].text)
-
-
-# ########################### Zips Lists Into One List ##########################################
-
-# zipper = zip(usernames, filtered)
-
-
-
-# ################################# Get the Word Count of an Inputted User #########################
-
-# def WordCount(username):
-# 	for a in range(len(zipper)):
-# 		if zipper[a][0] == username:
-# 			# print zipper[a][1]
-# 			splitList.append(str(zipper[a][1]).split())
-
-
-# time.sleep(10)
-# print("please type username")
-# userinput = raw_input()
-# WordCount(userinput)
-# flat_list = []
-# for sublist in splitList:
-# 	for item in sublist:
-# 		flat_list.append(item)
-# print len(flat_list)
